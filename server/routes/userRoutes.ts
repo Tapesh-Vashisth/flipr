2659e0cfcbd39b41c5240d3ddef66f7f7829c4ad
Prasad <prasadjs178@gmail.com>
2023-01-14 09:25:25 +0530

    minor changes

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import login from "../controllers/auth/login"
+import sendVerifyEmailOtp from '../controllers/auth/sendVerifyEmailOtp'
 import signup from "../controllers/auth/signup"
 import verifyJWT from "../middleware/verifyJWT"
 
@@ -7,7 +8,7 @@ const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
-router.get('/sendotp', login)
+router.post('/sendotp', sendVerifyEmailOtp)
 
 router.use('/jwt', verifyJWT)
 
