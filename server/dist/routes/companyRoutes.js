9f3a1c0b7aed6ef1a8517a05457ca3fdce61c40e
tapesh <tapeshvashisth789@gmail.com>
2023-01-16 18:51:35 +0530

    added verification route in company

--- server/dist/routes/companyRoutes.js
+++ server/dist/routes/companyRoutes.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const companyController_1 = require("../controllers/company/companyController");
+const verifyJWT_1 = __importDefault(require("../middleware/verifyJWT"));
 const router = express_1.default.Router();
+router.use(verifyJWT_1.default);
 router.get('/set', companyController_1.setCompanyDetails);
 router.get('/', companyController_1.getCompanyDetails);
 exports.default = router;
